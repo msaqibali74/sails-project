59e05939555cd06d4bf126375f49575627273b79
msaqibali74 <msaqibali74@gmail.com>
2019-12-01 01:26:13 +0400

    ckeditor

--- assets/ckeditor/plugins/zamanager/plugin.js
+++
@@ -1,6 +0,0 @@
-﻿CKEDITOR.plugins.add( 'zsmanager', {
-    init: function( editor ) {
-        editor.config.filebrowserBrowseUrl = './ckeditor/plugins/zsmanager/index.php';
-         editor.config.filebrowserImageBrowseUrl = './ckeditor/plugins/zsmanager/index.php';
-    }
-});
