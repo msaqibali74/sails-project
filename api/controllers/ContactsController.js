3fb1af9c5f8e6cfa03649634cdc3e3d564657a59
msaqibali74 <msaqibali74@gmail.com>
2019-12-07 01:10:10 +0400

    contact

--- api/controllers/ContactsController.js
+++ api/controllers/ContactsController.js
@@ -1,9 +1,18 @@
 module.exports = {
     index:async function(req,res){
+      /*
       var contactsListing = await Contacts.find({
         where: {owner:req.param('userid')},
         select: ['name','number', 'email']
       });
+      */
+     var contactsListing = await Contacts.find({where: {owner:req.param('userid')}}).populate("owner");
+     var contactsListing = await Contacts.find().populate("owner");
+     //res.json(contactsListing);
+     contactsListing.forEach(function(contact) {
+      console.log(contact.owner.username);
+     }); 
+     //console.log(contactsListing);
      res.view('pages/index',{cons: contactsListing}); 
     }
 };
